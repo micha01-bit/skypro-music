cf5638896b813fafcdcf182ed6dc5c11fabb3833
micha01-bit <ebespalov05@gmail.com>
2026-05-02 23:56:59 +0700

    Work-on-mistakes

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -16,7 +16,7 @@ export default function Bar() {
                   <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
                 </svg>
               </div>
-              <div className={'player__btnPlay btn'}>
+              <div className={classnames(styles.player__btnPlay, styles.btn)}>
                 <svg className={styles.player__btnPlaySvg}>
                   <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
                 </svg>
