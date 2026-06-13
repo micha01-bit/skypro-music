ecd032134880ed5b89b6759addfa950f278213af
micha01-bit <ebespalov05@gmail.com>
2026-06-13 21:34:02 +0700

    hw-7 commit-1

--- src/sharedTypes/sharedTypes.ts
+++ src/sharedTypes/sharedTypes.ts
@@ -8,7 +8,7 @@ export type TrackType = {
   album: string;
   logo: string | null;
   track_file: string;
-  staredUser: string[];
+  stared_user: string[];
 };
 
 export type CategoryType = {
@@ -16,27 +16,3 @@ export type CategoryType = {
   items: number[]; // ID треков, а не сами треки
   name: string;
 };
-
-
-
-
-// export type TrackType = {
-//   _id: number;
-//   name: string;
-//   author: string;
-//   release_date: string;
-//   genre: string[];
-//   duration_in_seconds: number;
-//   album: string;
-//   logo: string | null;
-//   track_file: string;
-//   staredUser: string[];
-// };
-
-// export type CategoryType = {
-//   _id: number;
-//   items: TrackType[]; // Уточняем: items — это массив треков
-//   name: string;
-// };
-
-// export type FavoriteType = TrackType[]; // Любимые треки — это массив TrackType
